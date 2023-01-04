ac54af6ba3996fdae2d73c6d65dab2892aa62ace
Kamil <pszpytma@itstream.pl>
2023-01-04 17:09:54 +0100

    zatw

--- WWW/ViewModels/DefaultViewModel.cs
+++ WWW/ViewModels/DefaultViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DotVVM.Framework.ViewModel;
+using WWW.Api;
 
 namespace WWW.ViewModels
 {
@@ -10,12 +11,15 @@ namespace WWW.ViewModels
     {
         public RequestSearch search { get; set; }
 
-		public int Refresh { get; set; }
-        public string Title { get; set;}
+        public int Refresh { get; set; }
+        public string Title { get; set; }
+
+        public System.Collections.Generic.ICollection<WeatherForecast> list {get;set;}
 
         public override Task Init()
         {
             search=new RequestSearch();
+            list = new List<WeatherForecast>();
             return base.Init();
 
         }
