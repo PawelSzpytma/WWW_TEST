664f471f90f0d0e5d646f34aa0776396ff3cc39f
Kamil <pszpytma@itstream.pl>
2023-02-02 08:23:59 +0100

    daty

--- WWW/Controls/Models/ModalOne.cs
+++ WWW/Controls/Models/ModalOne.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using WWW.Api;
 
 namespace WWW.Controls.Models
@@ -6,14 +7,23 @@ namespace WWW.Controls.Models
     public class ModalOne
     {
         public RequestSearch search { get; set; }
+
+        public DateTime? date { get; set; } 
+
         public ModalOne()
         {
             search = new RequestSearch();
             list = new List<WeatherForecast>();
+            date = DateTime.Now;
         }
 
         public bool Visible { get; set; }
         public int Refresh { get; set; }
         public System.Collections.Generic.ICollection<WeatherForecast> list { get; set; }
+
+        public void Save()
+        {
+
+        }
     }
 }
